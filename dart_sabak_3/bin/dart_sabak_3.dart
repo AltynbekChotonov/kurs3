736e5ce210e48e4abf0c85eb310f56ef0db1a992
AltynbekChotonov <dtvkanalnews@gmail.com>
2024-01-17 22:50:13 +0600

    koshuldu dart_sabak 3

---
+++ dart_sabak_3/bin/dart_sabak_3.dart
@@ -0,0 +1,38 @@
+void main(List<String> arguments) {
+  // List => [] tizme
+  List birList = [
+    'kours3',
+    15,
+    25.45,
+    'kours4',
+    15,
+    25.45,
+  ];
+  //print(birList[3]);
+
+  // Set => {} ушинтип атайбыз
+  Set birSet = {
+    'kours3',
+    15,
+    25.45,
+    'kours4',
+    15,
+    25.45,
+  };
+  // print(birSet);
+
+  // Map => {} (dictionary) создук
+
+  Map birMep = {
+    15: 'On besh',
+    'Hello': 'Салам',
+    'Name': 'Ат',
+  };
+  print(birMep['Hello']);
+
+  /*Key --------> value
+  Ачкыч --------> Маани
+  Hello --------> Салам
+  Name ---------> Ат
+  */
+}
